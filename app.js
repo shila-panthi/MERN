3d84fd69e4d0edcfa037f30078b91e78dae3122e
shila-panthi <pas075bei040@wrc.edu.np>
2023-09-24 22:20:10 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("hello world")
+console.log(2+3)
+const c="abcd";
+const  d="panthee";
+const m=5;
+const n=6;
+const sum= m+n
+console.log(sum)
+
+//array
+const numbers=[1,2,3,4,5]
+console.log(numbers.length) // for printing length
+console.log(numbers[1])
+
+
+var b=20;
+let o=20;
+
+//object
+const person ={
+    name:"shila"
+}
